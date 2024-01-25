ae3ed6082dfb70e6f7d4e7dd9444faa7fb9a8025
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-01-25 15:00:48 -0800

    shuffle function returns deck id

--- utils/shuffle.ts
+++ utils/shuffle.ts
@@ -1,5 +1,5 @@
 export default async function shuffle() {
   const response = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
   const data = await response.json()
-  const deckId = data.deck_id
+  return(data.deck_id)
 }
