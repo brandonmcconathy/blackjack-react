0924ac3244e021eea2bbe3f625b3508b892af30e
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-01-24 20:01:53 -0800

    Built more of the draw card function

--- utils/drawcard.ts
+++ utils/drawcard.ts
@@ -1,3 +1,5 @@
-export default function drawCard() {
-  
+export default async function drawCard(deckId:string) {
+  const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+  const data = await response.json()
+  return data.cards[0]
 }
