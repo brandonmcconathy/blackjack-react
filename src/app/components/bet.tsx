e32a72822e4d88870635bf69f300b322e2d4361a
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-02-02 13:00:38 -0800

    Added negative values to bet buttons and adjusted some styles

--- src/app/components/bet.tsx
+++ src/app/components/bet.tsx
@@ -19,7 +19,7 @@ export default function Bet() {
         setStage('play')
     }
 
-    const betValues = ['1','5','10','25']
+    const betValues = {positive: ['1','5','10','25'], negative: ['-1','-5','-10','-25']}
 
     return(
         <main className="flex flex-col gap-5 justify-center items-center text-center mt-16">
@@ -27,10 +27,10 @@ export default function Bet() {
             <h2 className="text-xl">{bet}</h2>
             <div className="flex flex-col justify-center items-center text-center gap-5">
                 <div className="flex justify-center items-center gap-5">
-                    {betValues.map((value) => <BetButton value={Number(value)} name='positive' setValidBet={setValidBet} bet={bet} setBet={setBet} balance={balance} key={value} />)}
+                    {betValues.positive.map((value) => <BetButton value={Number(value)} name='positive' setValidBet={setValidBet} bet={bet} setBet={setBet} balance={balance} key={value} />)}
                 </div>
                 <div className="flex justify-center items-center gap-5">
-                    {betValues.map((value) => <BetButton value={Number(value)} name='negative' setValidBet={setValidBet} bet={bet} setBet={setBet} balance={balance} key={value} />)}
+                    {betValues.negative.map((value) => <BetButton value={Number(value)} name='negative' setValidBet={setValidBet} bet={bet} setBet={setBet} balance={balance} key={value} />)}
                 </div>
             </div>
             {validBet ? 
@@ -66,7 +66,7 @@ function BetButton(props: {value:number, name:string, setValidBet:any, bet:numbe
             setBet((prevBet:any) => prevBet + value)
             setValidBet(true)
         } else {
-            setBet((prevBet:any) => prevBet - value)
+            setBet((prevBet:any) => prevBet + value)
             if (bet <= value) {
                 setBet(0)
                 setValidBet(false)
@@ -77,8 +77,8 @@ function BetButton(props: {value:number, name:string, setValidBet:any, bet:numbe
     return(
         <>
         {valid ? 
-            <button name={name} value={value} className="bg-amber-200 w-16 text-black rounded-full py-5 px-7 box-pop" onClick={handleBet}>{value}</button> :
-            <button name={name} value={value} className="bg-gray-500 w-16 text-black rounded-full py-5 px-7 box-pop" onClick={handleBet} disabled>{value}</button>}
+            <button name={name} value={value} className="bg-amber-200 text-xl w-24 h-24 text-black rounded-full py-5 px-7 box-pop text-center" onClick={handleBet}>{value}</button> :
+            <button name={name} value={value} className="bg-gray-500 text-xl w-24 h-24 text-black rounded-full py-5 px-7 box-pop text-center" onClick={handleBet} disabled>{value}</button>}
         </>
     )
 }
