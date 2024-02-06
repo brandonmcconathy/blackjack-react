b0b5e68147a9ed38f2d2dcc941a6450e2b3f00b9
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-02-06 10:43:42 -0800

    Starting building out start game sequence

--- src/app/components/play.tsx
+++ src/app/components/play.tsx
@@ -18,31 +18,55 @@ export default function Play() {
       const response = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
       const data = await response.json()
       setDeckId(data.deck_id)
+      return data.deck_id
     }
-    shuffle()
+
+    async function startGame() {
+      const deck_id = await shuffle()
+      console.log(deck_id)
+      setTimeout(() => {
+        drawPlayerCard(deck_id)
+        setTimeout(() => {
+          drawDealerCard(deck_id)
+          setTimeout(() => {
+            drawPlayerCard(deck_id)
+            setTimeout(() => {
+              drawDealerCard(deck_id)
+            },2000)
+          },2000)
+        },2000)
+      },2000)
+    }
+
+    startGame()
   },[])
 
-  const drawPlayerCard = async () => {
+  const drawPlayerCard = async (deckId:string) => {
     const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
     const data = await response.json()
     setPlayer((prevCards) => ([...prevCards, data.cards[0]]))
+    console.log('player')
   }
 
-  const drawDealerCard = async () => {
+  const drawDealerCard = async (deckId:string) => {
     const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
     const data = await response.json()
     setDealer((prevCards) => ([...prevCards, data.cards[0]]))
+    console.log('dealer')
   }
-  console.log(player)
+
+  const handleClick = () => {
+    drawPlayerCard(deckId)
+  }
 
   return(
     <main className="flex flex-col justify-evenly text-center h-screen">
       <h1>{balance}</h1>
       <h1>Dealer</h1>
-      <CardDisplay cards={player} />
       <CardDisplay cards={dealer} />
+      <CardDisplay cards={player} />
       <h1>Player</h1>
-      <button onClick={drawPlayerCard} className="bg-green-500 text-slate-800 px-3 py-1 rounded-xl text-xl font-semibold box-pop hover:bg-green-300 transition duration-300">Hit</button>
+      <button onClick={handleClick} className="bg-green-500 text-slate-800 px-3 py-1 rounded-xl text-xl font-semibold box-pop hover:bg-green-300 transition duration-300">Hit</button>
     </main>
   )
 }
