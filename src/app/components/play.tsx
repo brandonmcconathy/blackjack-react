226382de75830029f77f29273b993e7c67c8eacb
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-02-13 12:31:35 -0500

    Started refactoring the draw card function

--- src/app/components/play.tsx
+++ src/app/components/play.tsx
@@ -62,6 +62,12 @@ export default function Play() {
     }
   }
 
+  const drawCard = async (deckId:string) => {
+    const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+    const data = await response.json()
+    return(data.cards[0])
+  }
+
   const drawPlayerCard = async (deckId:string) => {
     const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
     const data = await response.json()
