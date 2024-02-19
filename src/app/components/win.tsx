88101fc40fc3b0f19cfa2937c6ea7707d184ffa3
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-02-18 18:43:02 -0800

    Started win page

--- src/app/components/win.tsx
+++ src/app/components/win.tsx
@@ -1,5 +1,18 @@
+import { BetContext, BalanceContext } from "../../../lib/context"
+import { useContext } from "react"
+
 export default function Win() {
+
+  const contextBet:any = useContext(BetContext)
+  const contextBalance:any = useContext(BalanceContext)
+  const { bet, setBet } = contextBet
+  const { balance, setBalance } = contextBalance
+
   return(
-    <h1>Win</h1>
+    <section>
+      <h1>You Win!</h1>
+      <h2>+ {bet}</h2>
+      <h3>New Balance: {balance}</h3>
+    </section>
   )
 }
