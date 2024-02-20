8d79e6d6d88e17d6a583c66c5c3d45d9a9380ea2
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-02-20 14:33:50 -0800

    Added bet reset and balance increase to win page

--- src/app/components/win.tsx
+++ src/app/components/win.tsx
@@ -1,5 +1,5 @@
 import { BetContext, BalanceContext } from "../../../lib/context"
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 
 export default function Win() {
 
@@ -7,11 +7,21 @@ export default function Win() {
   const contextBalance:any = useContext(BalanceContext)
   const { bet, setBet } = contextBet
   const { balance, setBalance } = contextBalance
+  
+  const [ betAmount, setBetAmount ] = useState(0)
+  const [ balanceAmount, setBalanceAmount ] = useState(0)
+
+  useEffect(() => {
+    setBetAmount(bet)
+    setBalanceAmount(balance)
+    setBet(0)
+    setBalance((prevBalance:number) => prevBalance + bet)
+  },[])
 
   return(
     <section>
       <h1>You Win!</h1>
-      <h2>+ {bet}</h2>
+      <h2>+ {betAmount}</h2>
       <h3>New Balance: {balance}</h3>
     </section>
   )
