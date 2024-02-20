80ff6f830af973bbbcbb572e2c361052325984b5
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-02-19 19:06:40 -0800

    Started lose page

--- src/app/components/lose.tsx
+++ src/app/components/lose.tsx
@@ -1,5 +1,15 @@
+import { BalanceContext } from "../../../lib/context"
+import { useContext } from "react"
+
 export default function Lose() {
+
+  const contextBalance:any = useContext(BalanceContext)
+  const { balance, setBalance } = contextBalance
+
   return(
-    <h1>Lose</h1>
+    <section>
+      <h1>You Lose</h1>
+      <h3>Balance: {balance}</h3>
+    </section>
   )
 }
