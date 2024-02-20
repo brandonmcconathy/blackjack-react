9f3a490ed96425d60d345a8a30fa90ea84ecc2e4
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-02-20 14:43:23 -0800

    Added buttons to play again on end game screens

--- src/app/components/lose.tsx
+++ src/app/components/lose.tsx
@@ -1,10 +1,12 @@
-import { BalanceContext, BetContext } from "../../../lib/context"
+import { BalanceContext, BetContext, StageContext } from "../../../lib/context"
 import { useContext } from "react"
 
 export default function Lose() {
 
   const contextBalance:any = useContext(BalanceContext)
   const contextBet:any = useContext(BetContext)
+  const contextStage:any = useContext(StageContext)
+  const { setStage } = contextStage
   const { balance } = contextBalance
   const { setBet } = contextBet
 
@@ -14,6 +16,10 @@ export default function Lose() {
     <section>
       <h1>You Lose</h1>
       <h3>Balance: {balance}</h3>
+      <div>
+        <button onClick={function() {setStage('bet')}} className="bg-green-500 text-slate-800 px-4 py-1 rounded-xl text-2xl self-center font-semibold box-pop hover:bg-green-300 transition duration-300">Play Again</button>
+        <button onClick={function() {setStage('start')}} className="bg-green-500 text-slate-800 px-4 py-1 rounded-xl text-2xl self-center font-semibold box-pop hover:bg-green-300 transition duration-300">Leave</button>
+      </div>
     </section>
   )
 }
--- src/app/components/win.tsx
+++ src/app/components/win.tsx
@@ -1,12 +1,14 @@
-import { BetContext, BalanceContext } from "../../../lib/context"
+import { BetContext, BalanceContext, StageContext } from "../../../lib/context"
 import { useContext, useEffect, useState } from "react"
 
 export default function Win() {
 
   const contextBet:any = useContext(BetContext)
   const contextBalance:any = useContext(BalanceContext)
+  const contextStage:any = useContext(StageContext)
   const { bet, setBet } = contextBet
   const { balance, setBalance } = contextBalance
+  const { setStage } = contextStage
   
   const [ betAmount, setBetAmount ] = useState(0)
   const [ balanceAmount, setBalanceAmount ] = useState(0)
@@ -23,6 +25,10 @@ export default function Win() {
       <h1>You Win!</h1>
       <h2>+ {betAmount}</h2>
       <h3>New Balance: {balance}</h3>
+      <div>
+        <button onClick={function() {setStage('bet')}} className="bg-green-500 text-slate-800 px-4 py-1 rounded-xl text-2xl self-center font-semibold box-pop hover:bg-green-300 transition duration-300">Play Again</button>
+        <button onClick={function() {setStage('start')}} className="bg-green-500 text-slate-800 px-4 py-1 rounded-xl text-2xl self-center font-semibold box-pop hover:bg-green-300 transition duration-300">Leave</button>
+      </div>
     </section>
   )
 }
