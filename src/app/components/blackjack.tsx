110fb5e39d12781e0bf72f10137b11862d2a34b8
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-02-20 19:46:03 -0800

    Styled end game pages

--- src/app/components/blackjack.tsx
+++ src/app/components/blackjack.tsx
@@ -26,11 +26,11 @@ export default function Blackjack() {
   },[])
 
   return(
-    <section>
-      <h1>Blackjack!</h1>
-      <h2>+ {betAmount}</h2>
-      <h3>New Balance: {balance}</h3>
-      <div>
+    <section className="flex flex-col items-center justify-center mt-5 gap-6">
+      <h1 className="text-3xl">BLACKJACK</h1>
+      <h2 className="text-xl">+ {betAmount}</h2>
+      <h3 className="text-xl">Balance: {balance}</h3>
+      <div className="flex gap-6">
         <button onClick={function() {setStage('bet')}} className="bg-green-500 text-slate-800 px-4 py-1 rounded-xl text-2xl self-center font-semibold box-pop hover:bg-green-300 transition duration-300">Play Again</button>
         <button onClick={function() {setStage('start')}} className="bg-green-500 text-slate-800 px-4 py-1 rounded-xl text-2xl self-center font-semibold box-pop hover:bg-green-300 transition duration-300">Leave</button>
       </div>
