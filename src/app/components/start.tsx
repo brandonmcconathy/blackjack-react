149ed1c37db6de9e18ef0ce7fda306df9c7f4d08
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-02-23 20:45:48 -0800

    Added cards to title screen

--- src/app/components/start.tsx
+++ src/app/components/start.tsx
@@ -10,6 +10,10 @@ export default function Start() {
     <main className="text-center mt-10">
       <h1 className="text-5xl mb-16">Blackjack</h1>
       <button onClick={function() {setStage('bet')}} className="bg-green-500 text-slate-800 px-3 py-1 rounded-xl text-xl font-semibold box-pop hover:bg-green-300 transition duration-300">Start Game</button>
+      <div>
+        <img src="https://deckofcardsapi.com/static/img/KS.png" />
+        <img src="https://deckofcardsapi.com/static/img/AS.png" />
+      </div>
     </main>
   )
 }
--- utils/drawcard.ts
+++ utils/drawcard.ts
@@ -2,5 +2,6 @@ export default async function drawCard(deckId:string, down:boolean) {
     const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
     const data = await response.json()
     data.cards[0].down = down
+    console.log(data.cards[0])
     return(data.cards[0])
 }
