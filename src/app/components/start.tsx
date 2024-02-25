c6d7be2f8758ad7dcb4681d5fb29bf81c1ade956
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-02-24 21:27:29 -0800

    Styled title screen

--- src/app/components/start.tsx
+++ src/app/components/start.tsx
@@ -10,9 +10,9 @@ export default function Start() {
     <main className="text-center mt-10">
       <h1 className="text-5xl mb-16">Blackjack</h1>
       <button onClick={function() {setStage('bet')}} className="bg-green-500 text-slate-800 px-3 py-1 rounded-xl text-xl font-semibold box-pop hover:bg-green-300 transition duration-300">Start Game</button>
-      <div>
-        <img src="https://deckofcardsapi.com/static/img/KS.png" />
-        <img src="https://deckofcardsapi.com/static/img/AS.png" />
+      <div className="flex items-center justify-center mt-20">
+        <img className="w-48 -mr-32" src="https://deckofcardsapi.com/static/img/KS.png" />
+        <img className="w-48 -ml-10" src="https://deckofcardsapi.com/static/img/AS.png" />
       </div>
     </main>
   )
