617cd39b6817fe11fcf5a1ab143c8e695ba6f238
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-02-29 21:14:13 -0800

    Styled bet screen for small screens

--- src/app/components/bet.tsx
+++ src/app/components/bet.tsx
@@ -81,8 +81,8 @@ function BetButton(props: {value:number, name:string, setValidBet:any, bet:numbe
     return(
         <>
         {valid ? 
-            <button name={name} value={value} className="bg-amber-200 text-xl w-24 h-24 text-black rounded-full py-5 px-7 box-pop text-center hover:bg-amber-400 transition duration-500" onClick={handleBet}>{value}</button> :
-            <button name={name} value={value} className="bg-gray-500 text-xl w-24 h-24 text-black rounded-full py-5 px-7 box-pop text-center transition duration-500" onClick={handleBet} disabled>{value}</button>}
+            <button name={name} value={value} className="bg-amber-200 text-xl w-12 h-12 sm:w-24 sm:h-24 text-black rounded-full box-pop text-center hover:bg-amber-400 transition duration-500" onClick={handleBet}>{value}</button> :
+            <button name={name} value={value} className="bg-gray-500 text-xl w-12 h-12 sm:w-24 sm:h-24 text-black rounded-full box-pop text-center transition duration-500" onClick={handleBet} disabled>{value}</button>}
         </>
     )
 }
